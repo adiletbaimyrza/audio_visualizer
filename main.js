ccb58c960e385602b2cecdd80df6abe399ff574b
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-05-08 15:23:31 +0200

    implementing new player

--- main.js
+++ main.js
@@ -1,113 +1,176 @@
-const audioPlayer = document.getElementById('audio-player')
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-const audioSource = audioCtx.createMediaElementSource(audioPlayer)
-let analyser = audioCtx.createAnalyser()
-analyser.fftSize = 128
-let dataArray = null
+const audioElement = document.getElementById("audio-element");
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const audioSource = audioCtx.createMediaElementSource(audioElement);
+let analyser = audioCtx.createAnalyser();
+analyser.fftSize = 128;
+let dataArray = new Uint8Array(analyser.frequencyBinCount);
 
-const ROW_LENGTH = 64
-const COL_LENGTH = 30
-const SONG_NAMES = ['adele', 'mirbek', 'skryptonite']
+const ROW_LENGTH = 64;
+const COL_LENGTH = 30;
+const SONG_NAMES = ["adele", "mirbek", "skryptonite"];
+
+const state = {
+  isPlaying: false,
+};
+
+const playPauseButton = document.getElementById("play-pause-button");
+const playPauseButtonImage = playPauseButton.querySelector("img");
+const forwardStepButton = document.getElementById("forward-step-button");
+const backwardStepButton = document.getElementById("backward-step-button");
+const controlsProgressBar = document.getElementById("controls__progress-bar");
+const controlsProgressBarFilled = controlsProgressBar.querySelector(".progress-bar__filled");
 
 let createGrid = (gridContainerId, generateId, iStart, iEnd, jStart, jEnd) => {
-  const gridContainer = document.getElementById(gridContainerId)
+  const gridContainer = document.getElementById(gridContainerId);
   for (let i = iStart; i !== iEnd; iEnd > iStart ? i++ : i--) {
     for (let j = jStart; j !== jEnd; jEnd > jStart ? j++ : j--) {
-      const gridItem = document.createElement('div')
-      gridItem.classList.add('grid-item')
-      gridItem.setAttribute('id', generateId(i, j))
-      gridContainer.appendChild(gridItem)
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridItem.setAttribute("id", generateId(i, j));
+      gridContainer.appendChild(gridItem);
     }
   }
-}
+};
 
 let createGrids = () => {
   createGrid(
-    'left-grid-container',
+    "left-grid-container",
     (i, j) => `left-${i}-${j}`,
     COL_LENGTH - 1,
     -1,
     ROW_LENGTH - 1,
     -1
-  )
-  createGrid('right-grid-container', (i, j) => `right-${i}-${j}`, COL_LENGTH - 1, -1, 0, ROW_LENGTH)
+  );
   createGrid(
-    'bottom-left-grid-container',
+    "right-grid-container",
+    (i, j) => `right-${i}-${j}`,
+    COL_LENGTH - 1,
+    -1,
+    0,
+    ROW_LENGTH
+  );
+  createGrid(
+    "bottom-left-grid-container",
     (i, j) => `bottom-left-${i}-${j}`,
     0,
     COL_LENGTH,
     ROW_LENGTH - 1,
     -1
-  )
+  );
   createGrid(
-    'bottom-right-grid-container',
+    "bottom-right-grid-container",
     (i, j) => `bottom-right-${i}-${j}`,
     0,
     COL_LENGTH,
     0,
     ROW_LENGTH
-  )
-}
+  );
+};
 
 let getHslColor = (index, soundIntensity) => {
-  const hue = (360 / 30) * index
-  const saturation = '100%'
-  const lightness = 50 + (soundIntensity / 30) * 50 + '%'
-  const color = `hsl(${hue}, ${saturation}, ${lightness})`
-  return color
-}
+  const hue = (360 / 30) * index;
+  const saturation = "100%";
+  const lightness = 50 + (soundIntensity / 30) * 50 + "%";
+  const color = `hsl(${hue}, ${saturation}, ${lightness})`;
+  return color;
+};
 
 let updateGrids = () => {
-  analyser.getByteFrequencyData(dataArray)
+  analyser.getByteFrequencyData(dataArray);
   for (let j = 0; j < ROW_LENGTH; j++) {
-    const soundIntensity = Math.floor(dataArray[j] / 20)
+    const soundIntensity = Math.floor(dataArray[j] / 20);
     for (let i = 0; i < COL_LENGTH; i++) {
-      const color = getHslColor(i, soundIntensity)
+      const color = getHslColor(i, soundIntensity);
       document.getElementById(`left-${i}-${j}`).style.backgroundColor =
-        i < soundIntensity ? color : '#000'
+        i < soundIntensity ? color : "#000";
       document.getElementById(`right-${i}-${j}`).style.backgroundColor =
-        i < soundIntensity ? color : '#000'
+        i < soundIntensity ? color : "#000";
       document.getElementById(`bottom-left-${i}-${j}`).style.backgroundColor =
-        i < soundIntensity ? color : '#000'
+        i < soundIntensity ? color : "#000";
       document.getElementById(`bottom-right-${i}-${j}`).style.backgroundColor =
-        i < soundIntensity ? color : '#000'
+        i < soundIntensity ? color : "#000";
     }
   }
-  requestAnimationFrame(updateGrids)
-}
+  requestAnimationFrame(updateGrids);
+};
 
-document.getElementById('audio-file').addEventListener('change', function () {
-  audioUrl = URL.createObjectURL(this.files[0])
-  audioPlayer.src = audioUrl
+document.getElementById("audio-file").addEventListener("change", function () {
+  audioUrl = URL.createObjectURL(this.files[0]);
+  audioElement.src = audioUrl;
 
-  const bufferLength = analyser.frequencyBinCount
-  dataArray = new Uint8Array(bufferLength)
+  const bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
 
-  audioSource.connect(analyser)
-  analyser.connect(audioCtx.destination)
-})
+  audioSource.connect(analyser);
+  analyser.connect(audioCtx.destination);
+});
 
 SONG_NAMES.forEach((song) => {
-  document.getElementById(song).addEventListener('click', () => {
-    if (audioCtx.state === 'suspended') {
-      audioCtx.resume()
+  document.getElementById(song).addEventListener("click", () => {
+    if (audioCtx.state === "suspended") {
+      audioCtx.resume();
     }
-    if (!audioPlayer.paused) {
-      audioPlayer.pause()
-      audioPlayer.currentTime = 0
+    if (!audioElement.paused) {
+      audioElement.pause();
+      audioElement.currentTime = 0;
     }
-    audioPlayer.src = `/audio_equalizer/audio/${song}.mp3`
-    const bufferLength = analyser.frequencyBinCount
-    dataArray = new Uint8Array(bufferLength)
+    audioElement.src = `/audio/${song}.mp3`;
+    const bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
 
-    audioSource.connect(analyser)
-    analyser.connect(audioCtx.destination)
-    audioPlayer.play()
-  })
-})
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    audioElement.play();
+  });
+});
 
-document.getElementById('audio-player').addEventListener('play', () => {
-  updateGrids()
-})
+document.getElementById("audio-player").addEventListener("play", () => {
+  updateGrids();
+});
 
-createGrids()
+audioElement.addEventListener("play", () => {
+  updateGrids();
+});
+
+playPauseButton.addEventListener("click", () => {
+  state.isPlaying = !state.isPlaying;
+
+  playPauseButtonImage.src = state.isPlaying ? "/svg/pause.svg" : "/svg/play.svg";
+
+  if (state.isPlaying) {
+    if (audioCtx.state === "suspended") {
+      audioCtx.resume();
+    }
+    if (!audioElement.paused) {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+    }
+    const bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+
+    audioElement.play();
+  } else {
+    audioElement.pause();
+  }
+});
+
+forwardStepButton.addEventListener("click", () => {
+  audioElement.currentTime += 15;
+});
+
+backwardStepButton.addEventListener("click", () => {
+  audioElement.currentTime -= 15;
+});
+
+audioElement.addEventListener("timeupdate", () => {
+  progressUpdate();
+});
+
+function progressUpdate() {
+  const percent = (audioElement.currentTime / audioElement.duration) * 100;
+  controlsProgressBarFilled.style.flexBasis = `${percent}%`;
+}
+
+createGrids();
