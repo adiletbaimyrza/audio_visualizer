c597d9d7a7bcac5e4f177312e5a8c06e54512dac
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-07-13 14:06:32 +0200

    change poster image formats

--- main.js
+++ main.js
@@ -168,7 +168,7 @@ const populatePlaylist = () => {
       </td>
 
       <td class="poster-cell">
-        <img src="/audio_visualizer/posters/${song.path}.jpg" alt="${
+        <img src="/audio_visualizer/posters/${song.path}.webp" alt="${
       song.name
     } by ${song.artist}" />
       </td>
@@ -304,7 +304,7 @@ ui.forwardStepBtn.addEventListener("click", () => {
 
     ui.audio.play();
 
-    ui.currSong.poster.src = `/audio_visualizer/posters/${nextSong.path}.jpg`;
+    ui.currSong.poster.src = `/audio_visualizer/posters/${nextSong.path}.webp`;
     ui.currSong.poster.alt = `${nextSong.name} by ${nextSong.artist}`;
     ui.currSong.name.textContent = nextSong.name;
     ui.currSong.artist.textContent = nextSong.artist;
@@ -385,7 +385,7 @@ ui.backwardStepBtn.addEventListener("click", () => {
 
     ui.audio.play();
 
-    ui.currSong.poster.src = `/audio_visualizer/posters/${prevSong.path}.jpg`;
+    ui.currSong.poster.src = `/audio_visualizer/posters/${prevSong.path}.webp`;
     ui.currSong.poster.alt = `${prevSong.name} by ${prevSong.artist}`;
     ui.currSong.name.textContent = prevSong.name;
     ui.currSong.artist.textContent = prevSong.artist;
@@ -728,7 +728,7 @@ songs.forEach((song) => {
 
         ui.audio.play();
 
-        ui.currSong.poster.src = `/audio_visualizer/posters/${song.path}.jpg`;
+        ui.currSong.poster.src = `/audio_visualizer/posters/${song.path}.webp`;
         ui.currSong.poster.alt = `${song.name} by ${song.artist}`;
         ui.currSong.name.textContent = song.name;
         ui.currSong.artist.textContent = song.artist;
@@ -804,7 +804,7 @@ songs.forEach((song) => {
 });
 
 const initializeCurrSongData = () => {
-  ui.currSong.poster.src = `/audio_visualizer/posters/${songs[0].path}.jpg`;
+  ui.currSong.poster.src = `/audio_visualizer/posters/${songs[0].path}.webp`;
   ui.currSong.poster.alt = `${songs[0].name} by ${songs[0].artist}`;
   ui.currSong.name.textContent = `${songs[0].name}`;
   ui.currSong.artist.textContent = songs[0].artist;
